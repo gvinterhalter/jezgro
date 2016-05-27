c2c302686e5d0e8a828d89531da929396ae4012f
Marko Rankovic <marko.rankovic@outlook.com>
2016-05-27 11:08:08 +0200

    Started multi-file commit

--- clang-cpp/clg.cpp
+++ clang-cpp/clg.cpp
@@ -1,12 +1,15 @@
 #include <clang-c/Index.h>
 
+#include <algorithm>
 #include <map>
 #include <utility>
 #include <iostream>
+#include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 #include "utils.hpp"
-
 
 struct Symbol
 {
@@ -23,10 +26,69 @@ struct Context
   CXCursor callerCursor;
 };
 
+struct Insertion
+{
+    std::string name;
+    std::string version;
+    unsigned offset;
+};
+
 std::map<std::string, Symbol> symbolMap;
+std::vector<Insertion> insertionList;
 CXCursor rootCursor;
 CXTranslationUnit tu = 0;
 CXFile file = 0;
+
+bool compare (Insertion first, Insertion second)
+{
+    return first.offset < second.offset;
+}
+
+void makeChangesToFile(const char * filename)
+{
+  std::ifstream infile (filename, std::ifstream::binary);
+  std::string output;
+  std::stringstream outfile(output);
+  //std::ofstream outfile ((filename +"_new.cpp"), std::ofstream::out);
+  std::vector<Insertion>::iterator it = insertionList.begin();
+  unsigned offset = 0;
+
+  infile.seekg (0,infile.end);
+  long size = infile.tellg();
+  infile.seekg (0);
+
+  char* buffer = new char[size];
+
+  while (it != insertionList.end())
+  {
+    Insertion next = *it;
+
+    infile.read(buffer, (next.offset - offset + next.name.length()));
+    outfile.write(buffer, (next.offset - offset + next.name.length()));
+    outfile.write(next.version.data(), next.version.length());
+    offset = next.offset + next.name.length();
+
+    it++;
+  }
+
+  infile.read(buffer, (size - offset));
+  outfile.write(buffer, (size - offset));
+  outfile.flush();
+  delete[] buffer;
+
+  std::cout<< "\n\n\n==========================================================================================" << std::endl;
+  std::cout<< "==========================================================================================" << std::endl;
+  std::cout<< " Changed file : "<< filename << std::endl;
+  std::cout<< "==========================================================================================" << std::endl;
+  std::cout<<  (outfile.str()) << std::endl;
+  std::cout<< "==========================================================================================" << std::endl;
+  std::cout<< "==========================================================================================" << std::endl;
+
+
+  //outfile.close();
+  infile.close();
+
+}
 
 void changeRefName(CXCursor cursor, std::string name, unsigned version)
 {
@@ -42,9 +104,15 @@ void changeRefName(CXCursor cursor, std::string name, unsigned version)
   }
   else
   {
+    Insertion ins;
+
+    ins.name = name;
+    ins.version = "__" + std::to_string(version);
+    ins.offset = offset;
+
+    insertionList.push_back(ins);
     std::cout << "\tApplying change to: " << name << "__" << version
-              << " at line " << line << " : " << column << std::endl
-              << "\tOffset: "<< offset << " in buffer" << std::endl;
+              << " at line " << line << " : " << column << std::endl;
   }
 }
 
@@ -65,7 +133,9 @@ enum CXVisitorResult   visitReference (void *context, CXCursor cursor, CXSourceR
   return CXVisit_Continue;
 };
 
-unsigned int varDecl(CXCursor cursor){
+unsigned int varDecl(CXCursor cursor)
+{
+  Context context;
   std::string spelling= getCursorSpelling(cursor);
   std::string typeName = getCursorTypeName(cursor);
   std::map<std::string, Symbol>::iterator it = symbolMap.find(spelling);
@@ -73,26 +143,26 @@ unsigned int varDecl(CXCursor cursor){
   if (it == symbolMap.end())
   {
     symbolMap.insert(std::pair<std::string, Symbol> (spelling, {spelling, typeName, 0}));
+    context.callerCursor = cursor;
+    context.name = spelling;
+    context.version = 0;
   }
   else
   {
-    //Uziamo podatke iz pronadjenog elementa, uklanjamo ga i ubacujemo azuriranu verziju
-    Context context;
     std::pair <std::string, Symbol> resultPair = *it;
     Symbol result = resultPair.second;
 
     result.version ++;
-    symbolMap.erase(it);
+
     context.callerCursor = cursor;
     context.name = spelling;
     context.version = result.version;
 
-
     symbolMap.insert(std::pair<std::string, Symbol>(spelling, {spelling, typeName, result.version}));
     symbolMap.erase(it);
-
-    clang_findReferencesInFile(cursor, file, {(void*) &context, visitReference});
   }
+
+  clang_findReferencesInFile(cursor, file, {(void*) &context, visitReference});
 
   return 0;
 }
@@ -137,27 +207,29 @@ enum CXChildVisitResult mainVisitor( CXCursor cursor, CXCursor parent, CXClientD
   if ( ! clang_Location_isFromMainFile( clang_getCursorLocation(cursor) ) )
     return CXChildVisit_Continue;
 
-
   // Ako je root CXCursor parent, to znaci da je u pitanju globalna stvar
-  if ( !clang_equalCursors( parent, rootCursor ) )
-    printCursorInfo(cursor, false);
-  else
+  if (clang_equalCursors( parent, rootCursor ) )
+  {
     printCursorInfo(cursor, true);
-
-  CXCursorKind kind = clang_getCursorKind ( cursor );
-  // obradjujemo deklaracije variabli i funkcija
-  if (kind == CXCursor_VarDecl || kind == CXCursor_ParmDecl)
-    varDecl (cursor);
-  else if (kind == CXCursor_FunctionDecl)
-    fnDecl (cursor);
+    CXCursorKind kind = clang_getCursorKind ( cursor );
+    // obradjujemo deklaracije variabli i funkcija
+    if (kind == CXCursor_VarDecl)
+      varDecl (cursor);
+    else if (kind == CXCursor_FunctionDecl)
+      fnDecl (cursor);
+  }
+  else
+    printCursorInfo(cursor, false);
 
   return CXChildVisit_Recurse;
 }
 
 int main( int argc, char** argv )
 {
+  insertionList= std::vector<Insertion>();
   if( argc < 2 )
     return -1;
+
 
   CXIndex index = clang_createIndex( 1, 0 );
   tu = clang_parseTranslationUnit( index, argv[1], NULL, 0, NULL, 0, 0);
@@ -172,6 +244,10 @@ int main( int argc, char** argv )
   rootCursor = clang_getTranslationUnitCursor( tu );
 
   clang_visitChildren(rootCursor, mainVisitor, 0);
+
+  std::sort(insertionList.begin(), insertionList.end(), compare);
+
+  makeChangesToFile(argv[1]);
 
   clang_disposeTranslationUnit( tu );
   clang_disposeIndex( index );
